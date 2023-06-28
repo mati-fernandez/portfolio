3f8085098df1df7f96cc1c0ba7712ec74d6eb7f8
Matias Fernandez <69617430+mati-fernandez@users.noreply.github.com>
2023-06-28 08:49:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    document.addEventListener("click", (e) => {
+      if (e.target.matches("#first-page")) {
+          document.querySelector(".seccion-aptitudes").scrollIntoView();
+      };
+      if (e.target.matches("#second-page")) {
+          document.querySelector(".seccion-tecnologias").scrollIntoView();
+      };
+    });
+  profilePicSound();
+  document.addEventListener("click", (e) => {
+  if (e.target.matches("#a-ver")) {
+      window.scrollTo({
+          behavior: "smooth",
+          top: 0
+      });
+    }
+  });
+});
+const profilePicSound = () => {
+  let soundTempo;
+  const $audioToggleBtn = document.querySelector("#audio-toggle"),
+   audioOn = "🔊", audioOff = "🔇",
+   $sound = document.querySelector(".profile-audio");
+   document.addEventListener("click", (e) => {
+      if (e.target.matches("#audio-toggle")) {
+          if ($audioToggleBtn.textContent === audioOff) {
+            $audioToggleBtn.textContent = audioOn;
+          } else {
+            $audioToggleBtn.textContent = audioOff;
+          }
+      }
+    });
+   document.addEventListener("mouseover", (e) => {
+      if (e.target.matches("img#profile-pic")) {
+          document.getElementById("msje-condicional").style.display = "none";
+          if ($audioToggleBtn.textContent == audioOn) {
+            $sound.play();
+          }
+        document.addEventListener("mouseout", e => {
+          $sound.pause();
+        });
+      };
+    });
+}
