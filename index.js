c904ca12d536d36d06195f507674121dc4af073e
Matias Fernandez <matifer.dev@gmail.com>
2024-03-23 23:50:48 -0300

    New sfx and prof pic. New sound and light sparkle added on toggle img

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const d = document;
-//Establecer todo el js en la carga del dom:
+//Establecer todo el js después de la carga del dom:
 d.addEventListener('DOMContentLoaded', (e) => {
-  //Inicializando timeout global
+  //Variables de uso global:
   let timeout = 0;
   console.log('Timeout Initialized!');
+  //Selectores de uso global
+  const $cajaCentral = d.getElementById('caja-central'),
+    $cajaCara = d.querySelector('#caja-cara img:nth-child(2)'),
+    $audioEffect1 = d.createElement('audio'),
+    $audioToggleBtn = d.querySelector('#audio-toggle');
+  $audioEffect1.src = 'toggleImg.mp3';
+  d.body.appendChild($audioEffect1);
+
   //Evitar el long press en mobile
   d.addEventListener('contextmenu', function (event) {
     event.preventDefault();
     if (window.innerWidth > 630) {
       const guiterGif = d.getElementById('guitar-gif');
       guiterGif.style.display = 'block';
-      const cajaCentral = d.getElementById('caja-central');
-      const computedStyle = window.getComputedStyle(cajaCentral);
-      // Obtener el ancho exacto de cajaCentral en píxeles
-      const cajaCentralWidth = parseFloat(computedStyle.width);
-      // Establecer el ancho de gifAnimado con el ancho de cajaCentral
-      guiterGif.style.width = cajaCentralWidth + 'px';
+      const computedStyle = window.getComputedStyle($cajaCentral);
+
+      // Obtener el ancho exacto de $cajaCentral en píxeles
+      const $cajaCentralWidth = parseFloat(computedStyle.width);
+
+      // Establecer el ancho de gifAnimado con el ancho de $cajaCentral
+      guiterGif.style.width = $cajaCentralWidth + 'px';
       clearTimeout(timeout);
       console.log('Timeout cleared');
       timeout = setTimeout(() => {
@@ -24,8 +33,10 @@ d.addEventListener('DOMContentLoaded', (e) => {
       }, 3000);
     }
   });
+
   //Inicializando interval global
   let interval = 0;
+
   //Funcion de cambio de prof-pic
   const imgToggle = () => {
     const pp = d.querySelector('#profile-pic');
@@ -38,6 +49,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
       pph.src = 'prof-pic-hover.jpg';
     }
   };
+
   //Reemplazo de prof-pic por tiempo
   const imgInterval = (mode) => {
     clearInterval(interval);
@@ -49,21 +61,40 @@ d.addEventListener('DOMContentLoaded', (e) => {
     console.log('Interval Created!');
   };
   imgInterval('Create');
+
+  //Funcion de efecto en toggleImg
+  const imgToggleEffect = () => {
+    if ($audioToggleBtn.classList.contains('fa-volume-high')) {
+      $audioEffect1.play();
+    }
+    if (window.innerWidth > 630) {
+      $cajaCentral.style.boxShadow = '0 0 50px 1px white';
+      const timeout = setTimeout(() => {
+        $cajaCentral.style.boxShadow = '0 0 20px 10px rgba(0, 0, 0, 0.368)';
+      }, 200);
+    } else {
+      $cajaCentral.style.boxShadow = '0 0 20px 10px rgba(0, 0, 0, 0)';
+      $cajaCara.style.boxShadow = '0 0 20px 3px white';
+      const timeout = setTimeout(() => {
+        $cajaCara.style.boxShadow = '0 0 20px 10px rgba(0, 0, 0, 0.156)';
+      }, 200);
+    }
+  };
+
   //Manejo del boton change para prof-pic
   const $changeImg = d.querySelector('#img-toggle');
   d.addEventListener('click', (e) => {
     if (e.target.matches('#img-toggle')) {
       imgInterval('Create');
       imgToggle();
+      imgToggleEffect();
     }
   });
 
   //Manejo del botón de sonido
   const profilePicSound = () => {
-    let soundTempo;
-    const $audioToggleBtn = d.querySelector('#audio-toggle'),
-      $sound = d.querySelector('#profile-audio'),
-      $suggestiveArrow = d.querySelector('.fa-arrow-up');
+    const $sound = d.querySelector('#profile-audio'),
+      $suggestiveArrow = d.querySelector('#suggestive-arrow');
     d.addEventListener('click', (e) => {
       if (e.target.matches('#audio-toggle')) {
         $suggestiveArrow.style.display = 'none';
@@ -71,6 +102,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
         $audioToggleBtn.classList.toggle('fa-volume-xmark');
       }
     });
+
     //Manejo del sonido al hacer hover y msje condicional
     d.addEventListener('mouseover', (e) => {
       if (e.target.matches('img#profile-pic')) {
@@ -102,9 +134,18 @@ d.addEventListener('DOMContentLoaded', (e) => {
     }
   });
 
-  //
+  //Scroll to top del #a-ver
   d.addEventListener('click', (e) => {
     if (e.target.matches('#a-ver')) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: 0,
+      });
+    }
+  });
+  //Scroll to top general
+  d.addEventListener('click', (e) => {
+    if (e.target.matches('#scroll-to-top')) {
       window.scrollTo({
         behavior: 'smooth',
         top: 0,
